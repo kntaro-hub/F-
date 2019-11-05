88d32bb129400c399811c0c4782ae56e34c6d6fb
kntaro-hub <55696411+kntaro-hub@users.noreply.github.com>
2019-11-06 00:19:47 +0900

    2019/11/06 メッセージウィンドウ表示処理途中

---
+++ F+/Assets/Script/MessageWindow.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class MessageWindow : MonoBehaviour
+{
+    // テキストの基
+    [SerializeField] TextMeshProUGUI TextMeshPrefab;
+
+    // 表示しているテキスト
+    List<TextMeshProUGUI> texts = new List<TextMeshProUGUI>();
+
+    // 表示する予定のテキスト
+    List<TextMeshProUGUI> reserves = new List<TextMeshProUGUI>();
+
+    // テキストが流れている途中か
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            this.AddMessage("tarako", Color.red);
+        }
+    }
+
+
+    public void AddMessage(string text, Color color)
+    {
+        TextMeshProUGUI textMesh = Instantiate(TextMeshPrefab);
+        Vector3 InitPos = textMesh.transform.localPosition;
+
+        textMesh.transform.SetParent(this.transform);
+
+        textMesh.color = color;
+
+        textMesh.text = text;
+
+        foreach(var itr in texts)
+        {
+            itr.transform.DOLocalMoveY(itr.transform.localPosition.y + 60.0f, 0.5f);
+        }
+
+        if (texts.Count > 0)
+        {
+            textMesh.transform.localPosition = new Vector2(InitPos.x, texts[texts.Count - 1].transform.localPosition.y - 60.0f);
+        }
+        else
+        {
+            textMesh.transform.localPosition = new Vector2(InitPos.x, InitPos.y - 60.0f);
+        }
+
+        textMesh.transform.DOLocalMoveY(InitPos.y, 0.5f);
+
+        texts.Add(textMesh);
+    }
+
+    private IEnumerator TextFadeTimer()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+
+    }
+
+    #region singleton
+
+    static MessageWindow _instance;
+
+    public static MessageWindow instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                var previous = FindObjectOfType(typeof(MessageWindow));
+                if (previous)
+                {
+                    Debug.LogWarning("Initialized twice. Don't use MidiBridge in the scene hierarchy.");
+                    _instance = (MessageWindow)previous;
+                }
+                else
+                {
+                    var go = new GameObject("MessageWindow");
+                    _instance = go.AddComponent<MessageWindow>();
+                    DontDestroyOnLoad(go);
+                    go.hideFlags = HideFlags.HideInHierarchy;
+                }
+            }
+            return _instance;
+        }
+    }
+
+    #endregion
+}
--- F+/Assets/Script/PlayerControll.cs
+++ F+/Assets/Script/PlayerControll.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
+/// <summary>
+/// プレイヤーキャラクターの操作を管理
+/// </summary>
 public class PlayerControll : MonoBehaviour
 {
+    // 移動中フラグ
+    bool IsMove = false;
 
+    // 移動にかかる時間
+    [SerializeField] const float MoveTime = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,42 +23,62 @@ public class PlayerControll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // プレイヤーキャラクターの操作を一括管理する
         this.Controll();
     }
 
     private void Controll()
     {
-        if(Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            this.transform.DOMove(new Vector3(
-                this.transform.position.x + FieldInfomation.GridSize,
-                this.transform.position.y,
-                this.transform.position.z),
-                0.5f).SetEase(Ease.InOutCubic);
+        if (!IsMove)
+        {// 移動中でない場合移動できる
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {// 右
+                this.Move(new Vector3(1.0f, 0.0f, 0.0f) * FieldInfomation.GridSize);
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {// 左
+                this.Move(new Vector3(-1.0f, 0.0f, 0.0f) * FieldInfomation.GridSize);
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {// 奥
+                this.Move(new Vector3(0.0f, 0.0f, 1.0f) * FieldInfomation.GridSize);
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {// 手前
+                this.Move(new Vector3(0.0f, 0.0f, -1.0f) * FieldInfomation.GridSize);
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            this.transform.DOMove(new Vector3(
-                this.transform.position.x - FieldInfomation.GridSize,
-                this.transform.position.y,
-                this.transform.position.z),
-                0.5f).SetEase(Ease.InOutCubic);
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            this.transform.DOMove(new Vector3(
-                this.transform.position.x,
-                this.transform.position.y,
-                this.transform.position.z + FieldInfomation.GridSize),
-                0.5f).SetEase(Ease.InOutCubic);
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            this.transform.DOMove(new Vector3(
-                this.transform.position.x,
-                this.transform.position.y,
-                this.transform.position.z - FieldInfomation.GridSize),
-                0.5f).SetEase(Ease.InOutCubic);
-        }
+    }
+
+    /// <summary>
+    /// 移動用関数
+    /// </summary>
+    /// <param name="Direct">目的地</param>
+    private void Move(Vector3 Direct)
+    {
+        // MoveTime秒かけて目的地へ
+        this.transform.DOMove(new Vector3(
+                this.transform.position.x + Direct.x,
+                this.transform.position.y + Direct.y,
+                this.transform.position.z + Direct.z),
+                MoveTime).SetEase(Ease.InOutCubic);
+        // 移動中フラグon
+        IsMove = true;
+
+        // MoveTime秒経つまで次の入力を受け付けないようにする
+        StartCoroutine(MoveTimer());
+    }
+
+    /// <summary>
+    /// 指定の時間が経ったら入力を受け付けられるようにする
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator MoveTimer()
+    {
+        // MoveTime秒まつ
+        yield return new WaitForSeconds(MoveTime);
+
+        // 再入力可能状態へ
+        IsMove = false;
     }
 }
