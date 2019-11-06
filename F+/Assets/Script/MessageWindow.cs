125439205749e49289a7f386d662d77897deb4a8
kntaro-hub <55696411+kntaro-hub@users.noreply.github.com>
2019-11-06 12:23:39 +0900

    2019/11/06 メッセージ表示処理制作

--- F+/Assets/Script/MessageWindow.cs
+++ F+/Assets/Script/MessageWindow.cs
@@ -6,16 +6,30 @@ using DG.Tweening;
 
 public class MessageWindow : MonoBehaviour
 {
-    // テキストの基
-    [SerializeField] TextMeshProUGUI TextMeshPrefab;
+    // =--------- 構造体定義 ---------= //
+    // テキスト一つ分の情報
+    struct TextInformation
+    {
+        public string text;
+        public Color color;
+    }
+   
+    // =--------- SerializeField ---------= //
+    [SerializeField] TextMeshProUGUI TextMeshPrefab;    // テキストの基
+    [SerializeField] private float   FallSpeed = 0.3f;  // テキストが流れる速度（秒）
+    [SerializeField] private float   TextSpace = 60.0f; // テキスト同士の間隔
+    [SerializeField] private int     MaxText = 4;       // 一度に表示されるテキストの最大数
 
+    // =--------- 変数宣言 ---------= //
     // 表示しているテキスト
     List<TextMeshProUGUI> texts = new List<TextMeshProUGUI>();
 
     // 表示する予定のテキスト
-    List<TextMeshProUGUI> reserves = new List<TextMeshProUGUI>();
+    List<TextInformation> reserves = new List<TextInformation>();
 
-    // テキストが流れている途中か
+    // テキストを流せるか
+    private bool IsFallText = true;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -26,48 +40,95 @@ public class MessageWindow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            this.AddMessage("tarako", Color.red);
+        {// エディタ上では有効
+#if UNITY_EDITOR    
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                this.AddMessage("tarako", Color.red);
+            }
+#endif
+        }
+
+        if(IsFallText)
+        {// テキストを流せる状態なら
+            if(reserves.Count > 0)
+            {// かつ、予約テキストが一件でもあったら
+
+                // テキスト生成
+                TextMeshProUGUI textMesh = Instantiate(TextMeshPrefab);
+
+                // 初期座標保存
+                Vector3 InitPos = textMesh.transform.localPosition;
+
+                // 生成したテキストをメッセージウィンドウの子に設定
+                textMesh.transform.SetParent(this.transform);
+
+                // 予約テキストからテキストを更新
+                textMesh.text = reserves[reserves.Count - 1].text;
+                textMesh.color = reserves[reserves.Count - 1].color;
+
+                // 予約テキスト削除
+                reserves.RemoveAt(reserves.Count - 1);
+
+                foreach (var itr in texts)
+                {// 今までのテキストをすべて一つ上に流す
+                    itr.transform.DOLocalMoveY(itr.transform.localPosition.y + TextSpace, FallSpeed);
+                }
+
+                // 生成したテキストを初期位置の一行分下へ
+                textMesh.transform.localPosition = new Vector2(InitPos.x, InitPos.y - TextSpace);
+
+                // 初期位置へ動かす
+                textMesh.transform.DOLocalMoveY(InitPos.y, FallSpeed);
+
+                // 生成したテキストをリストへ追加
+                texts.Add(textMesh);
+
+                // テキストを流せない状態にする
+                IsFallText = false;
+
+                // テキストを流せるまでのタイマーをスタートする
+                StartCoroutine(TextFadeTimer());
+
+                if(texts.Count > MaxText)
+                {// テキストが最大量を超えたら
+
+                    // 一番古いテキストを透明に
+                    texts[0].DOColor(Color.clear, FallSpeed);
+                }
+            }
         }
     }
 
-
+    /// <summary>
+    /// メッセージ予約
+    /// </summary>
+    /// <param name="text">メッセージ内容</param>
+    /// <param name="color">メッセージ色</param>
     public void AddMessage(string text, Color color)
     {
-        TextMeshProUGUI textMesh = Instantiate(TextMeshPrefab);
-        Vector3 InitPos = textMesh.transform.localPosition;
-
-        textMesh.transform.SetParent(this.transform);
-
-        textMesh.color = color;
-
-        textMesh.text = text;
-
-        foreach(var itr in texts)
-        {
-            itr.transform.DOLocalMoveY(itr.transform.localPosition.y + 60.0f, 0.5f);
-        }
-
-        if (texts.Count > 0)
-        {
-            textMesh.transform.localPosition = new Vector2(InitPos.x, texts[texts.Count - 1].transform.localPosition.y - 60.0f);
-        }
-        else
-        {
-            textMesh.transform.localPosition = new Vector2(InitPos.x, InitPos.y - 60.0f);
-        }
-
-        textMesh.transform.DOLocalMoveY(InitPos.y, 0.5f);
-
-        texts.Add(textMesh);
+        TextInformation information = new TextInformation();
+        information.text = text;
+        information.color = color;
+        reserves.Add(information);
     }
 
     private IEnumerator TextFadeTimer()
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(FallSpeed);
 
+        // テキストを流せる状態にする
+        IsFallText = true;
 
+        if (texts.Count > MaxText)
+        {// テキストが最大数を超えたら
+
+            // テキスト削除
+            Destroy(texts[0].gameObject);
+
+            // リストからも削除
+            texts.RemoveAt(0);
+        }
     }
 
     #region singleton
