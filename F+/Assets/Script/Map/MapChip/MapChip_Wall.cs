d82d69d4a82081812ae8ce23f6befc82974182c0
kntaro-hub <55696411+kntaro-hub@users.noreply.github.com>
2019-12-15 16:27:33 +0900

    no message

---
+++ F+/Assets/Script/Map/MapChip/MapChip_Wall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapChip_Wall : MapChipBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 壁は何もしない
+    /// </summary>
+    public override void ActiveMapChip()
+    {
+        return;
+    }
+}
---
+++ F+/Assets/Script/ThrowObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ThrowObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(float time, Point point)
+    {
+        this.transform.DOMove(MapData.GridToWorld(point), time).SetEase(Ease.Linear);
+    }
+}
